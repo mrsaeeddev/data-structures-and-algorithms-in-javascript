e143c6b717c7906dff5e697dbe0d34d628bb36f2
mrsaeeddev <saeed_dev@yahoo.com>
2021-08-10 09:11:38 +0500

    implemented binary tree and binary search tree

---
+++ tree/applications/binarytree.js
@@ -0,0 +1,33 @@
+const LEFT = 0
+const RIGHT = 1
+
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.descendants = [];
+        this.parent = null
+    }
+
+    get left() {
+        return this.descendants[LEFT]
+    }
+
+    set left(node) {
+        this.descendants[LEFT] = node
+        if (node) {
+            node.parent = this
+        }
+    }
+
+    get right() {
+        return this.descendants[RIGHT]
+    }
+
+    set right() {
+        this.descendants[RIGHT] = node
+        if (node) {
+            node.parent = this
+        }
+    }
+
+}
